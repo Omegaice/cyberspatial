36630094f9f41eb1bb374498c9a802aba8476049
James Sweet <james.sweet88@googlemail.com>
2017-09-25 09:12:32 -0400

    Implement HTTPS through LetsEncrypt

--- terraform/kubernetes_ingress.tf
+++ terraform/kubernetes_ingress.tf
@@ -1,5 +1,17 @@
 resource "null_resource" "kubernetes_ingress_resource" {
   provisioner "local-exec" {
+    command = "kubectl apply -f lego/00-namespace.yaml"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f lego/configmap.yaml"
+  }
+
+  provisioner "local-exec" {
+    command = "kubectl apply -f lego/deployment.yaml"
+  }
+
+  provisioner "local-exec" {
     command = "kubectl apply -f terraform/kubernetes_ingress.yaml"
   }
 }
